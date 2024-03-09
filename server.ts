3003cb50c1f015dd01015dd88e7555b532e767ca
Bhargavi549 <bhargavidora222@gmail.com>
2024-03-09 17:00:56 +0530

    changes added

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import movieRouter from './src/routes/movieRoutes';
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
 
 app.use(express.json());
 app.use('/', movieRouter);
